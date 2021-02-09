dbe7f51ec839ec547e2160b8e0b93c55d9fbf4db
YAIR JIMENEZ <YJIMENEZ@ARKUSNEXUS.COM>
2021-02-09 14:31:55 -0600

    router path changed

--- src/routers/appRouter.js
+++ src/routers/appRouter.js
@@ -14,8 +14,8 @@ export const AppRouter = () => {
         <div>
           <Navbar></Navbar>
           <Switch>
-            <Route exact path="/Home" component={Home} />
-            <Route exact path="/" component={Home} />
+            <Route exact path="./Home" component={Home} />
+            <Route exact path="./" component={Home} />
           </Switch>
         </div>
       </Router>
